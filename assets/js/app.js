59533ef9145682edfd5c66eedba5b72a44025729
kashifali554 <kashifali874@gmail.com>
2017-02-21 14:40:13 -0800

    updated versions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,7 @@
 // sanity check
 console.log("app.js linked.");
+
+// var screenShoot = $(".screen-shot")
+//       for(var i = 0; i < screenShoot.length; i++){
+//         var screen = screenShoot[i];
+//         $("")
